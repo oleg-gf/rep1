53655c926c367bee8f9303dd107c93d72dbea629
oleg-gf <oleg_gf@mail.ru>
2017-05-09 13:24:50 +0300

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,14 @@
+export const calc = (sign, a, b) => {
+  switch (sign) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    default:
+      return NaN;
+  }
+};
