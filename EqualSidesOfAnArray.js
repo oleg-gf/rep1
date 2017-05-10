308a9ef102761c0d6efbdf66fab6ac86384e74a2
oleg-gf <oleg_gf@mail.ru>
2017-05-10 22:07:01 +0300

    Update EqualSidesOfAnArray.js

--- EqualSidesOfAnArray.js
+++ EqualSidesOfAnArray.js
@@ -5,11 +5,15 @@ function findEvenIndex(arr)
     sum1 += arr[i];
   }
   for (i = 0; i < arr.length; i++) {
-    sum2 += arr[i];
-    if (sum2 > sum1/2) {
+    if (sum2 === (sum1 - sum2 - arr[i])) {
       num = i;
       sum2 = 0;
+      break;
     }
+    sum2 += arr[i];
+  }
+  for (i = num + 1; i < arr.length; i++) {
+    sum2 += arr[i];
   }
   return sum2 === (sum1 - sum2 - arr[num]) ? num: -1;
 }
