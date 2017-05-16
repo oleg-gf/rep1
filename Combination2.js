4289afb43911cfce84e8f17a5d5b99ab05812ea7
oleg-gf <oleg_gf@mail.ru>
2017-05-17 00:45:50 +0300

    более оптимизированный вариант

---
+++ Combination2.js
@@ -0,0 +1,24 @@
+const Combination = (k, n) => {
+  const factorial = (f) => {
+    let add = f;
+    if (add === 1 || add === 0) {
+      return 1;
+    }
+    add--;
+    return factorial(add)*f;
+  };
+  const Permutation = (k, n) => {
+  let nk = n - k;
+  const iter = (current) => {
+    if (current === nk) {
+      return 1;
+    }
+    return current * iter(current - 1);
+  };
+  return iter(n);
+  
+};
+  return Permutation(k, n)/factorial(k);
+  
+};
+Combination(3,40);
