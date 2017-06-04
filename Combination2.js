94e8f2f51d47ae2571d795abe4a65718005b57e9
oleg-gf <oleg_gf@mail.ru>
2017-06-04 23:22:14 +0300

    Update Combination2.js

--- Combination2.js
+++ Combination2.js
@@ -8,16 +8,16 @@ const Combination = (k, n) => {
     return factorial(add)*f;
   };
   const Permutation = (k, n) => {
-  let nk = n - k;
-  const iter = (current) => {
-    if (current === nk) {
-      return 1;
-    }
-    return current * iter(current - 1);
+      let nk = n - k;
+      const iter = (current) => {
+          if (current === nk) {
+            return 1;
+          }
+          return current * iter(current - 1);
+        };
+       return iter(n);
+
   };
-  return iter(n);
-  
-};
   return Permutation(k, n)/factorial(k);
   
 };
