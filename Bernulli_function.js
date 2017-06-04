8754e7e62eb7ec22b4e65c3c98762fa386130019
oleg-gf <oleg_gf@mail.ru>
2017-06-04 23:23:50 +0300

    Сокращение факториалов

--- Bernulli_function.js
+++ Bernulli_function.js
@@ -6,8 +6,19 @@ const factorial = (f) => {
       mult--;
       return factorial(mult)*f;
 };
+ const Permutation = (k, n) => {
+      let nk = n - k;
+      const iter = (current) => {
+          if (current === nk) {
+            return 1;
+          }
+          return current * iter(current - 1);
+        };
+       return iter(n);
+
+ };
 const Combination = (k, n) => {
-   return factorial(n)/(factorial(n - k)*factorial(k));
+   return Permutation(k, n)/factorial(k);
 };
 const Bernulli = (k, n, p) => {
   return Combination(k, n)*(p**k)*((1 - p)**(n - k));
