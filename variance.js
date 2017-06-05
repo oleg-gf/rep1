baca5ff5ec4863e9a7ba684f932f695bf44120ce
oleg-gf <oleg_gf@mail.ru>
2017-06-05 18:24:21 +0300

    Дисперсия дискретных величин

---
+++ variance.js
@@ -0,0 +1,15 @@
+const variance = (arr) => {
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    x += arr[i];
+  }
+  x /= arr.length;
+  console.log(x);
+  let result = 0;
+  for (let j = 0; j < arr.length; j++) {
+    result += (arr[j] - x)**2;
+    console.log((arr[j] - x)**2);
+  }
+  return result;
+};
+variance ([5,3,4,5,3]);
