87ddc073a145f64f19b92f6123520702e3660f81
oleg-gf <oleg_gf@mail.ru>
2017-07-28 10:20:11 +0300

    Update isPalindrom.js

--- isPalindrom.js
+++ isPalindrom.js
@@ -1,5 +1,5 @@
 const isPalindrom = (str) => {
-  str = str.toLowerCase().match(/[a-z0-9]/gi);
+  str = str.toLowerCase().match(/[a-zа-я0-9]/gi);
   return str.join('') === str.reverse().join('');
 };
 isPalindrom ('А роза упала на лапу Азора');
