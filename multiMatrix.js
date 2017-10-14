72a160e69509c45af3625e6d21b8a11d7c4b4ffc
oleg-gf <oleg_gf@mail.ru>
2017-10-14 20:51:57 +0300

    Update multiMatrix.js

--- multiMatrix.js
+++ multiMatrix.js
@@ -1,32 +1,17 @@
-Object.prototype.clone = function() {
-//функция копирования массивов
-    var newObj = (this instanceof Array) ? [] : {};
-    for (i in this) {
-        if (i == 'clone') 
-            continue;
-        if (this[i] && typeof this[i] == "object") {
-            newObj[i] = this[i].clone();
-        } 
-        else 
-            newObj[i] = this[i]
-    } return newObj;
-};
 const multiMatrix = (a, b) => {
   if (a[0].length !== b.length) {
     return 'Матрицы не согласованы';
   }
   const result = a.clone();
   let sum = 0;
-  console.log(b[0], b[0][0], b[0][1]);
   for (let i = 0; i < a.length; i++) {
-    for (let h = 0; h < a[0].length; h++) {
+    result.push([]);
+    for (let h = 0; h < b[0].length; h++) {
       for (let j = 0; j < a[0].length; j++) {
-        console.log('a[',i,'][',j, '] = ',a[i][j], '| b[',j,'][',h,'] = ', b[j][h])
         sum += a[i][j] * b[j][h];
       }
       result[i][h] = sum;
       sum = 0;
-      console.log(a, result);
     }
   }
   return result;
