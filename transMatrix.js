e4fd10534ccc9ac83102630401ead01394d8fc3b
oleg-gf <oleg_gf@mail.ru>
2017-10-15 08:45:46 +0300

    Update transMatrix.js

--- transMatrix.js
+++ transMatrix.js
@@ -4,14 +4,11 @@ const transMatrix = (a) => {
   const result = [];
   const iter3 = (i, j) => {
           if (j >= arows)  {return result;}
-          console.log('a[',j,'][',i,'] = ', a[j][i]);
           result[i][j] = a[j][i];
           return iter3(i, j + 1);
         };
   const iter1 = (i) => {
-    if (i >= acols) {
-      return result;
-      }
+    if (i >= acols) {return result;}
     result.push([]);
     iter3(i, 0);
     return iter1(i + 1);
